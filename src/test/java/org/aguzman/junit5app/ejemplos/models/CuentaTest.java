2c45007e8ee76de476fb1ae492d3b296bb5dda6a
Cristobal Gonzalez Gallego <cristobal.gonzalez-gallego.external@capgemini.com>
2025-05-06 09:58:33 +0200

    Primer commit

---
+++ src/test/java/org/aguzman/junit5app/ejemplos/models/CuentaTest.java
@@ -0,0 +1,22 @@
+package org.aguzman.junit5app.ejemplos.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaTest {
+
+    @Test
+    void testNombreCuenta() {
+        Cuenta cuenta = new Cuenta("Cristobal", new BigDecimal("1000.12345")); // Creo una cuenta con el nombre Cristobal y saldo 1000.00
+        //cuenta.setPersona("Cristobal"); // Establezco el nombre de la persona como Cristobal
+        String esperado = "Cristobal"; // Espero que el nombre de la persona sea Cristobal
+        String actual = cuenta.getPersona(); // Obtengo el nombre de la persona
+
+        Assertions.assertEquals(esperado,actual); // Comparo el nombre esperado con el actual
+    }
+
+}cd
